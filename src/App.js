fd2434dfcc279042afdf58d9673be840f9e5f19a
SrivastavaAman8604 <aman.srivastava2639@gmail.com>
2024-04-23 20:29:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+// import {QRCodeCanvas,QRCodeSVG} from 'qrcode.react'
+import QRCode from 'qrcode.react';
 
 function App() {
+  const [qrcode,setQrcode] = useState(null);
+  
+  const handleqr=(e)=>{
+    setQrcode(e.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>QRcode</h1>
+     <input type="text" onChange={handleqr} value={qrcode}/>
+     <div style={{marginTop:'20px'}}>
+      {/* <QRCodeCanvas value={qrcode}/>
+      <QRCodeSVG value={qrcode}/> */}
+      <QRCode value={qrcode}/>
+     </div>
     </div>
   );
 }
